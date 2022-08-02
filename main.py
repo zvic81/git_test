b962fb4723f3db59f0821f5304c4f902d885f43c
vic <zvic@list.ru>
2022-08-02 19:53:08 +0300

    test version

---
+++ main.py
@@ -0,0 +1,23 @@
+﻿mfile = open('self-test.txt', 'r')
+sfile=open('copy.txt','w')
+sfile.write(mfile.read())
+
+'''
+коментарий
+test comennt
+арара
+
+мчсмч
+
+
+'''
+mfile.close()
+sfile.close()
+mfile = open('self-test.txt', 'r')
+sfile=open('copy.txt','r')
+mas1=mfile.read()
+mas2=sfile.read()
+print(str(len(mas1)),str(len(mas2)))
+
+mfile.close()
+sfile.close()
